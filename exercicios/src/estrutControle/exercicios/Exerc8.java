5b7fdfd71e9b4cc237d4a5773a31bc29c1c5167a
Cleber Santos <clebersantos7@yahoo.com.br>
2021-07-28 23:21:11 -0300

    second commit java 2021

---
+++ exercicios/src/estrutControle/exercicios/Exerc8.java
@@ -0,0 +1,25 @@
+package estrutControle.exercicios;
+
+import java.util.Scanner;
+
+public class Exerc8 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Digite uma palavra: ");
+		Scanner scn = new Scanner(System.in);
+
+		String p = scn.next();
+
+		int ql = p.length();
+
+		for (int i = 1; i <= ql; i++) {
+
+			String pp = p.substring(i-1, i);
+			System.out.println(pp);
+
+		}
+
+	}
+
+}
---
+++ exercicios/src/estrutControle/exercicios/Exerc9.java
@@ -0,0 +1,29 @@
+package estrutControle.exercicios;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exerc9 {
+
+	public static void main(String[] args) {
+
+		List<Integer> num = new ArrayList<Integer>();
+
+		Scanner scn = new Scanner(System.in);
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println("Digite 1 número: ");
+			int n = scn.nextInt();
+
+			num.add(n);
+			
+		}
+		
+		System.out.println("O maior numero digitado foi: " + Collections.max(num));
+
+	}
+
+}
